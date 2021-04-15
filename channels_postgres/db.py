194f5973418ea8fc6fd685c625b79c12103a1c2b
Daniel Osaetin <osaetindaniel@gmail.com>
2021-04-15 11:30:56 +0100

    refactor: Improve send_to_channel method

--- channels_postgres/db.py
+++ channels_postgres/db.py
@@ -1,5 +1,6 @@
 """common db methods."""
 
+import random
 import asyncio
 import logging
 
@@ -13,41 +14,6 @@ class DatabaseLayer:
 
         if not self.logger:
             self.logger = logging.getLogger('channels_postgres.database')
-
-    async def send_to_channel(self, conn, group_key, message, expire, channel=None):
-        """Send a message on a channel."""
-        cur = await conn.cursor()
-        if channel is None:
-            channels = await self._retrieve_group_channels(cur, group_key)
-        else:
-            channels = [channel]
-
-        insert_message_sql = (
-            'INSERT INTO channels_postgres_message (channel, message, expire) VALUES '
-        )
-
-        last_channel = channels.pop()
-
-        values = []
-        for channel in channels:
-            insert_message_sql = insert_message_sql + "(%s, %s, (NOW() + INTERVAL '%s seconds')), "
-            values.extend((channel, message, expire))
-
-        insert_message_sql = insert_message_sql + "(%s, %s, (NOW() + INTERVAL '%s seconds'));"
-        values.extend((last_channel, message, expire))
-
-        await cur.execute(insert_message_sql, values)
-
-    async def add_channel_to_group(self, conn, group_key, channel, expire):
-        group_add_sql = (
-            'INSERT INTO channels_postgres_groupchannel (group_key, channel, expire) '
-            "VALUES (%s, %s, (NOW() + INTERVAL '%s seconds'))"
-        )
-
-        cur = await conn.cursor()
-        await cur.execute(group_add_sql, (group_key, channel, expire))
-
-        self.logger.debug('Channel %s added to Group %s', channel, group_key)
 
     async def _retrieve_group_channels(self, cur, group_key):
         retrieve_channels_sql = (
@@ -63,22 +29,58 @@ class DatabaseLayer:
 
         return channels
 
+    async def send_to_channel(self, conn, group_key, message, expire, channel=None):
+        """Send a message to a channel/channels (if no channel is specified)."""
+        cur = await conn.cursor()
+        if channel is None:
+            channels = await self._retrieve_group_channels(cur, group_key)
+        else:
+            channels = [channel]
+
+        values_str = b','.join(
+            cur.mogrify(
+                "(%s, %s, (NOW() + INTERVAL '%s seconds'))", (channel, message, expire)
+            ) for channel in channels
+        )
+        insert_message_sql = (
+            b'INSERT INTO channels_postgres_message (channel, message, expire) VALUES ' + values_str
+        )
+
+        await cur.execute(insert_message_sql)
+
+    async def add_channel_to_group(self, conn, group_key, channel, expire):
+        group_add_sql = (
+            'INSERT INTO channels_postgres_groupchannel (group_key, channel, expire) '
+            "VALUES (%s, %s, (NOW() + INTERVAL '%s seconds'))"
+        )
+
+        cur = await conn.cursor()
+        await cur.execute(group_add_sql, (group_key, channel, expire))
+
+        self.logger.debug('Channel %s added to Group %s', channel, group_key)
+
     async def delete_expired_groups(self, db_params):
+        expire = 60 * random.randint(10, 20)
+        self.logger.debug('Deleting expired groups in %s seconds...', expire)
+
         await asyncio.sleep(60)
         delete_sql = (
             'DELETE FROM channels_postgres_groupchannel '
             'WHERE expire < NOW()'
         )
-        conn = await aiopg.connect(**db_params)
-        cur = await conn.cursor()
-        await cur.execute(delete_sql)
+        async with aiopg.connect(**db_params) as conn:
+            cur = await conn.cursor()
+            await cur.execute(delete_sql)
 
     async def delete_expired_messages(self, db_params):
+        expire = 60 * random.randint(10, 20)
+        self.logger.debug('Deleting expired messages in %s seconds...', expire)
+
         await asyncio.sleep(60)
         delete_sql = (
             'DELETE FROM channels_postgres_message '
             'WHERE expire < NOW()'
         )
-        conn = await aiopg.connect(**db_params)
-        cur = await conn.cursor()
-        await cur.execute(delete_sql)
+        async with aiopg.connect(**db_params) as conn:
+            cur = await conn.cursor()
+            await cur.execute(delete_sql)
